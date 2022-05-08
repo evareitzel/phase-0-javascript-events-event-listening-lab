f41c81d651f65703b9bf3631c98afc377193d7d6
Eva Reitzel <evareitzel@gmail.com>
2022-05-08 19:20:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+    alert('Hee hee, that tickles!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
